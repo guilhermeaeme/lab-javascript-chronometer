aa1c5755879512d9147e93cd466eb24a4b031c81
guilhermeaeme <guilhermeaeme@gmail.com>
2018-11-23 22:07:08 -0200

    Iteration 1: The Logic

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,7 +1,43 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+	this.currentTime = 0,
+	this.intervalId = null,
 
-// }
+	this.startClick = function() {
+		var that = this;
+		this.intervalId = setInterval(function(){
+			that.currentTime += 1;
+			that.setTime();
+		}, 1000);
+	},
+
+	this.setMinutes = function() {
+		return Math.floor(this.currentTime / 60);
+	},
+
+	this.setSeconds = function() {
+		return this.currentTime - (this.setMinutes() * 60);
+	},
+
+	this.twoDigitsNumber = function(value) {
+		var twoDigits = '00' + value;
+		return twoDigits.substr(twoDigits.length - 2, 2);
+	},
+
+	this.setTime = function() {
+		this.minutes = this.twoDigitsNumber(this.setMinutes())
+		this.seconds = this.twoDigitsNumber(this.setSeconds());
+	},
+
+	this.stopClick = function() {
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	},
+
+	this.resetClick = function() {
+		this.currentTime = 0;
+	}
+}
 
 // Chronometer.prototype.startClick = function () {
 
